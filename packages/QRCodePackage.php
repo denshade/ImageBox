f31256e84ae992a319ea6c555b487e27e2ccdfe1
Vaneeckhaute Lieven <lveeckha@ai.internal.uzgent.be>
2016-08-21 21:59:44 +0200

    QRCodes.

---
+++ packages/QRCodePackage.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of QRCodePackage
+ *
+ * @author lveeckha
+ */
+class QRCodePackage {
+    public function register(\Slim\App $app) {
+        $app->get('/qrcode/{text}', function ($request, $response, $args) {
+            $text = $args['text'];
+            $response = $response->withHeader('Content-Description', 'File Transfer')
+                ->withHeader('Content-Type', 'application/octet-stream')
+                ->withHeader('Content-Disposition', 'attachment;filename="qrcode.png"')
+                ->withHeader('Expires', '0')
+                ->withHeader('Cache-Control', 'must-revalidate')
+                ->withHeader('Pragma', 'public')
+                ->withHeader('Content-Length', 365 );
+            QRcode::png($text);
+            return $response;
+        });
+        
+        $app->get('/qrscan/{file}', function ($request, $response, $args) {
+            $file = "qrcode.png";
+            $QRCodeReader = new Libern\QRCodeReader\QRCodeReader();
+            $qrcode_text = $QRCodeReader->decode($file);
+            echo $qrcode_text;
+        });
+    }
+}
